f84ecfb0046b125f3242887f0bf3a62c6a50ce7f
Sohan Rout <sohanrout84@gmail.com>
2026-03-04 16:43:16 +0530

    Fix : fixed hero and usp section

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Hero from "./components/Hero";
 import StatsStrip from "./components/StatsStrip";
 import Grid from "./components/ui/grid";
 import ImageCarousel from "./components/ui/infiniteCarousal";
-import Feature from "./components/Feature";
 import HowItWorks from "./components/HowItWorks";
 import CuetCoverage from "./components/CuetCoverage";
 import Courses from "./components/Courses";
@@ -11,7 +10,6 @@ import Usp from "./components/Usp";
 import Reviews from "./components/Review";
 import Faq from "./components/faq";
 import Footer from "./components/Footer";
-import PreferenceSupport from "./components/PreferenceSupport";
 
 export default function Home() {
   const logos = ["/logos/du.png", "/logos/srcc.png", "/logos/jnu.png", "/logos/st.png"];
@@ -25,22 +23,18 @@ export default function Home() {
         <Grid />
       </div>
 
-      <div className="max-w-6xl mx-auto">
+      <div className="max-w-6xl mx-auto pb-12">
         <div className="flex flex-col items-center justify-center">
           <h2 className="py-8 text-xl text-black">Colleges to Crack</h2>
           <ImageCarousel images={logos} speed={120} />
         </div>
       </div>
 
-      <div className="py-8">
-        <Feature />
+      <div className="rounded-t-4xl border-t border-t-neutral-50">
+        <HowItWorks />
       </div>
 
-      <HowItWorks />
-
       <CuetCoverage />
-
-      <PreferenceSupport />
 
       <Courses />
 
