620922f295c07784ec84c1f98e51768a274f7daf
Sohan Rout <sohanrout84@gmail.com>
2026-03-08 23:04:11 +0530

    Feat : added some changes to mock interface

--- app/mock-tests/[test-id]/page.tsx
+++ app/mock-tests/[test-id]/page.tsx
@@ -25,7 +25,7 @@ export default async function TestInstructionsPage({
           // no-op
         },
       },
-    }
+    },
   );
 
   // Get current user
@@ -71,7 +71,7 @@ export default async function TestInstructionsPage({
             // no-op
           },
         },
-      }
+      },
     );
 
     const {
@@ -96,42 +96,106 @@ export default async function TestInstructionsPage({
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-neutral-100 p-6">
-      <div className="max-w-2xl w-full rounded-2xl bg-white p-8 shadow-xl">
-        <h1 className="text-2xl font-bold text-center mb-4">{test.title}</h1>
+      <div className="max-w-6xl w-full rounded-2xl bg-white p-8 shadow-xl pb-40">
+        <h1 className="text-2xl font-semibold border-b">General Instruction</h1>
+        <ul className="space-y-2 p-6 text-neutral-600 list-decimal">
+          <li>
+            Total Duration of the examination is defined per mock(usually 60
+            minutes)
+          </li>
+          <li>
+            The clock will be set at the server. The countdown timer in the top
+            right corner of the screen will display the remaining time available
+            for you to complete the examination. When the timer reaches zero,
+            the examination will end itself. You will not be required to end or
+            submit your examination.
+          </li>
+          <li>
+            The question palette displayed on the right side of screen will show
+            the status of each question using one of the following symbols:
+            <div className="bg-neutral-100 flex flex-col my-4 gap-4 p-4 rounded-lg border border-neutral-200">
+              <div className="flex gap-4 items-center">
+                <div className="px-4 rounded-lg py-2 bg-white border border-neutral-200">
+                  1
+                </div>
+                <span>You have not visited the question yet.</span>
+              </div>
+              <div className="flex gap-4 items-center">
+                <div className="px-4 py-2 rounded-t-full bg-red-500 text-white">
+                  2
+                </div>
+                <span>You have not answered the question</span>
+              </div>
+              <div className="flex gap-4 items-center">
+                <div className="px-4 py-2 rounded-t-full bg-green-500 text-white">
+                  3
+                </div>
+                <span>You have answered the question.</span>
+              </div>
+              <div className="flex gap-4 items-center">
+                <div className="px-4 rounded-full py-2 bg-purple-600 text-white">
+                  4
+                </div>
+                <span>
+                  The question(s) marked for review will not be considered for
+                  evaluation
+                </span>
+              </div>
+              <div className="flex gap-4 items-center">
+                <div className="px-4 relative rounded-full py-2 text-white bg-purple-600">
+                  5
+                  <span className="absolute top-7 right-[-2] w-3 h-3 bg-green-500 rounded-full"></span>
+                </div>
+                <span>
+                  The question(s) aswered and marked for review will be
+                  considered for evaluation.
+                </span>
+              </div>
+            </div>
+          </li>
+          <h1 className="underline underline-black underline-offset-2 text-black text-2xl font-semibold">Navigating to a question:</h1>
+          <li>
+            To answer a question do the following :
+              <ul className="list-disc px-4">
+                <li>Click on the question number in the question palette at the right of your screen to go to that numbered question directly.</li>
+                <li>Click on Save & Next to save your answer for the current question and then go to the next question.</li>
+                <li>Click on Mark for Review & Next to save your answer for the current question, mark for review, and then fo to the next question.</li>
+              </ul>
+          </li>
+          <h1 className="underline underline-black underline-offset-2 text-black text-2xl font-semibold">Answering a Question:</h1>
+          <li>Procedure for answering a multiple choice type question:</li>
+        </ul>
 
-        <div className="space-y-2 text-gray-600">
-          <p>
-            <strong className="text-black">Year:</strong> {test.year}
-          </p>
-          <p>
-            <strong>Duration:</strong> {test.duration_minutes} minutes
-          </p>
-          <p>
-            <strong>Total Questions:</strong> {count}
-          </p>
-          <p>
-            <strong>Total Marks:</strong> {test.total_marks}
-          </p>
-        </div>
+        <form
+          action={startTest}
+          className="fixed bottom-0 items-center justify-center left-0 w-full bg-white border-t border-neutral-300 p-4 flex flex-col gap-4"
+        >
+          <label className="flex items-center py-2 gap-3 text-md max-w-6xl text-black">
+            <input
+              id="confirmStart"
+              name="confirmStart"
+              type="checkbox"
+              required
+              className="w-6 h-6 rouned-lg"
+            />
+            I have read and understood the instruction. All computer hardware alloted to me are in proper working condition. I declare that I am not in possession of / not wearing / not carrying any prohibited gadget like mobile phone, bluetooth devices etc. I agree that in case of not adhering to the instructions, I shall be liable to be debarded from this test and/or to disciplinary action which may include ban from future Tests/Examinations.
+          </label>
 
-        <div className="mt-6">
-          <h2 className="font-semibold mb-2">Instructions:</h2>
-          <ul className="list-disc list-inside space-y-1 text-sm text-gray-600">
-            <li>Each question carries allotted marks.</li>
-            <li>Negative marking applies where specified.</li>
-            <li>Test will auto-submit when time expires.</li>
-            <li>Do not refresh or close the browser during the test.</li>
-          </ul>
-        </div>
+          <div className="flex gap-4 max-w-6xl mx-auto w-full">
+            <button
+              type="submit"
+              className="w-full bg-emerald-300 text-black border py-3 rounded-lg hover:opacity-90 transition"
+            >
+              Proceed
+            </button>
 
-        <form action={startTest} className="mt-8 flex gap-4">
-          <button
-            type="submit"
-            className="w-full bg-emerald-300 text-black border py-3 rounded-lg hover:opacity-90 transition"
-          >
-            Start Test
-          </button>
-          <Link className="w-full flex items-center justify-center bg-red-200 text-black border py-3 rounded-lg hover:opacity-90 transition" href="/mock-tests">Go Back</Link>
+            <Link
+              className="w-full flex items-center justify-center bg-red-200 text-black border py-3 rounded-lg hover:opacity-90 transition"
+              href="/mock-tests"
+            >
+              Go Back
+            </Link>
+          </div>
         </form>
       </div>
     </div>
