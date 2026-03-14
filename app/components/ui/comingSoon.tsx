3535fb2526829bb5e49fbe7f1038f04c1546ae58
Sohan Rout <sohanrout84@gmail.com>
2026-03-14 23:42:33 +0530

    Feat : added coming soon

--- app/components/ui/comingSoon.tsx
+++ app/components/ui/comingSoon.tsx
@@ -5,7 +5,7 @@ export default function ComingSoon(){
         <main className="h-[50vh] flex flex-col items-center gap-4 justify-center">
             <span className="text-6xl animate-bounce">👍</span>
             <span className="uppercase text-lg font-light">We{"\'"}re Still</span>
-            <h1 className="text-4xl">Cooking Our Website</h1>
+            <h1 className="text-4xl">Preparing magic for your dreams</h1>
             <p className="text-lg text-neutral-500 w-xl text-center">We are going to publish this section soon. Meanwhile take a seat and relax.</p>
             <div className="inline-flex p-0.5 rounded-full bg-linear-to-br from-emerald-400 via-purple-600 to-emerald-400">
                 <Link
